1419059351f5986688408879f42e9197bc60fcde
Stanislav Ilin <youngfrezyx@gmail.com>
2024-09-27 13:09:51 +0500

    Update packages

--- test/infinite_date_timeline/easy_infinite_date_timeline_test.dart
+++ test/infinite_date_timeline/easy_infinite_date_timeline_test.dart
@@ -1,5 +1,4 @@
 import 'package:easy_date_timeline/src/easy_infinite_date_time/easy_infinite_date_timeline.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import '../testable_widget.dart';
@@ -43,29 +42,6 @@ void main() {
 
       await tester.tap(find.text('5'));
       expect(selectedDate, DateTime(2022, 1, 5));
-    });
-
-    testWidgets('Renders custom header using headerBuilder',
-        (WidgetTester tester) async {
-      await tester.pumpWidget(
-        TestableWidget(
-          child: EasyInfiniteDateTimeLine(
-            firstDate: DateTime(2022, 1, 1),
-            focusDate: DateTime(2022, 1, 15),
-            lastDate: DateTime(2022, 12, 31),
-            headerBuilder: (BuildContext context, DateTime date) {
-              return SizedBox(
-                height: 50,
-                child: Text(date.day.toString()),
-              );
-            },
-          ),
-        ),
-      );
-
-      expect(find.byType(SizedBox), findsOneWidget);
-      // one for the header and one for the selected date
-      expect(find.text('15'), findsExactly(2));
     });
   });
 }
